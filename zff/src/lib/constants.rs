eec124f569a663c9ba5f594ca7d3d3b36cc1dedd
ph0llux <ph0llux@pm.me>
2021-08-11 23:32:35 +0200

    various constant values added

---
+++ zff/src/lib/constants.rs
@@ -0,0 +1,14 @@
+// identifier: built as murmurhash with 525627482 as optional seed
+pub const HEADER_IDENTIFIER_DESCRIPTION_HEADER: u32 = 0xc735ff41;
+
+// Magic bytes
+pub const MAIN_HEADER_MAGIC: u32 = 0x7A66666d;
+
+
+// Encoding keys
+pub const ENCODING_KEY_CASE_NUMBER: &str = "cn";
+pub const ENCODING_KEY_EVIDENCE_NUMBER: &str = "ev";
+pub const ENCODING_KEY_EXAMINER_NAME: &str = "ex";
+pub const ENCODING_KEY_NOTES: &str = "no";
+pub const ENCODING_KEY_ACQISITION_DATE: &str = "ad";
+pub const ENCODING_KEY_SYSTEM_DATE: &str = "sd";
