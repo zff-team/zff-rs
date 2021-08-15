a73e17ac74159d3c48d08a82f75c6ac92581bb45
ph0llux <ph0llux@pm.me>
2021-08-15 23:34:25 +0200

    identifier for main/description and split -header added

--- zff/src/lib/constants.rs
+++ zff/src/lib/constants.rs
@@ -1,9 +1,7 @@
-// identifier: built as murmurhash with 525627482 as optional seed
-pub const HEADER_IDENTIFIER_DESCRIPTION_HEADER: u32 = 0xc735ff41;
-
-// Magic bytes
-pub const MAIN_HEADER_MAGIC: u32 = 0x7A66666d;
-
+// identifier: magic bytes
+pub const HEADER_IDENTIFIER_MAIN_HEADER: u32 = 0x7A66666d;
+pub const HEADER_IDENTIFIER_DESCRIPTION_HEADER: u32 = 0x7A666664;
+pub const HEADER_IDENTIFIER_SPLIT_HEADER: u32 = 0x7A666673;
 
 // Encoding keys
 pub const ENCODING_KEY_CASE_NUMBER: &str = "cn";
@@ -11,4 +9,3 @@ pub const ENCODING_KEY_EVIDENCE_NUMBER: &str = "ev";
 pub const ENCODING_KEY_EXAMINER_NAME: &str = "ex";
 pub const ENCODING_KEY_NOTES: &str = "no";
 pub const ENCODING_KEY_ACQISITION_DATE: &str = "ad";
-pub const ENCODING_KEY_SYSTEM_DATE: &str = "sd";
