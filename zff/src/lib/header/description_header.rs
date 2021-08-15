73b484b357ada63916cdf82a0ae6e22d6b59fb96
ph0llux <ph0llux@pm.me>
2021-08-15 23:35:08 +0200

    new_empty() and various set-methods added

--- zff/src/lib/header/description_header.rs
+++ zff/src/lib/header/description_header.rs
@@ -11,9 +11,9 @@ use crate::{
 	ENCODING_KEY_EXAMINER_NAME,
 	ENCODING_KEY_NOTES,
 	ENCODING_KEY_ACQISITION_DATE,
-	ENCODING_KEY_SYSTEM_DATE,
 };
 
+#[derive(Debug,Clone)]
 pub struct DescriptionHeader {
 	header_version: u8,
 	case_number: Option<String>,
@@ -21,11 +21,10 @@ pub struct DescriptionHeader {
 	examiner_name: Option<String>,
 	notes: Option<String>,
 	acquisition_date: Option<u32>,
-	system_date: Option<u32>
 }
 
 impl DescriptionHeader {
-	pub fn new(header_version: u8) -> DescriptionHeader {
+	pub fn new_empty(header_version: u8) -> DescriptionHeader {
 		Self {
 			header_version: header_version,
 			case_number: None,
@@ -33,11 +32,14 @@ impl DescriptionHeader {
 			examiner_name: None,
 			notes: None,
 			acquisition_date: None,
-			system_date: None,
 		}
 	}
 	pub fn header_version(&self) -> &u8 {
 		&self.header_version
+	}
+
+	pub fn set_case_number<V: Into<String>>(&mut self, value: V) {
+		self.case_number = Some(value.into())
 	}
 	pub fn case_number(&self) -> Option<&str> {
 		match &self.case_number {
@@ -45,11 +47,19 @@ impl DescriptionHeader {
 			None => None
 		}
 	}
+
+	pub fn set_evidence_number<V: Into<String>>(&mut self, value: V) {
+		self.evidence_number = Some(value.into())
+	}
 	pub fn evidence_number(&self) -> Option<&str> {
 		match &self.evidence_number {
 			Some(x) => Some(x),
 			None => None
 		}
+	}
+
+	pub fn set_examiner_name<V: Into<String>>(&mut self, value: V) {
+		self.examiner_name = Some(value.into())
 	}
 	pub fn examiner_name(&self) -> Option<&str> {
 		match &self.examiner_name {
@@ -57,20 +67,22 @@ impl DescriptionHeader {
 			None => None
 		}
 	}
+
+	pub fn set_notes<V: Into<String>>(&mut self, value: V) {
+		self.notes = Some(value.into())
+	}
 	pub fn notes(&self) -> Option<&str> {
 		match &self.notes {
 			Some(x) => Some(x),
 			None => None
 		}
 	}
+
+	pub fn set_acquisition_date(&mut self, value: u32) {
+		self.acquisition_date = Some(value)
+	}
 	pub fn acquisition_date(&self) -> Option<u32> {
 		match &self.acquisition_date {
-			Some(x) => Some(*x),
-			None => None
-		}
-	}
-	pub fn system_date(&self) -> Option<u32> {
-		match &self.system_date {
 			Some(x) => Some(*x),
 			None => None
 		}
@@ -88,20 +100,17 @@ impl HeaderObject for DescriptionHeader {
 		if let Some(case_number) = self.case_number() {
 			vec.append(&mut case_number.encode_for_key(ENCODING_KEY_CASE_NUMBER));
 		};
-		if let Some(evidence_number) = self.case_number() {
+		if let Some(evidence_number) = self.evidence_number() {
 			vec.append(&mut evidence_number.encode_for_key(ENCODING_KEY_EVIDENCE_NUMBER));
 		};
-		if let Some(examiner_name) = self.case_number() {
+		if let Some(examiner_name) = self.examiner_name() {
 			vec.append(&mut examiner_name.encode_for_key(ENCODING_KEY_EXAMINER_NAME));
 		};
-		if let Some(notes) = self.case_number() {
+		if let Some(notes) = self.notes() {
 			vec.append(&mut notes.encode_for_key(ENCODING_KEY_NOTES));
 		};
-		if let Some(acquisition_date) = self.case_number() {
+		if let Some(acquisition_date) = self.acquisition_date() {
 			vec.append(&mut acquisition_date.encode_for_key(ENCODING_KEY_ACQISITION_DATE));
-		};
-		if let Some(system_date) = self.case_number() {
-			vec.append(&mut system_date.encode_for_key(ENCODING_KEY_SYSTEM_DATE));
 		};
 		vec
 	}
@@ -112,13 +121,19 @@ impl HeaderEncoder for DescriptionHeader {
 		let mut vec = Vec::new();
 		let mut encoded_header = self.encode_header();
 		let identifier = Self::identifier();
-		let encoded_header_length = encoded_header.len() as u32;
-		
-		vec.push(ValueType::Object.as_raw_value());
-		vec.append(&mut identifier.to_le_bytes().to_vec());
+		let encoded_header_length = encoded_header.len() as u64;
+		vec.append(&mut identifier.to_be_bytes().to_vec());
 		vec.append(&mut encoded_header_length.to_le_bytes().to_vec());
 		vec.append(&mut encoded_header);
 
+		vec
+	}
+	fn encode_for_key<K: Into<String>>(&self, key: K) -> Vec<u8> {
+		let mut vec = Vec::new();
+		let mut encoded_key = Self::encode_key(key);
+		vec.append(&mut encoded_key);
+		vec.push(ValueType::Object.as_raw_value());
+		vec.append(&mut self.encode_directly());
 		vec
 	}
 }
