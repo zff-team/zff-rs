2130c2adcf1c870851b78f4249a3c73bba8e45f4
ph0llux <ph0llux@pm.me>
2021-08-17 16:11:02 +0200

    initial implementation of own error-type

---
+++ zff/src/lib/error.rs
@@ -0,0 +1,74 @@
+// - STD
+use std::fmt;
+use std::io;
+
+// - internal
+
+
+/// The main error-type of this crate.
+#[derive(Debug, Clone)]
+pub struct ZffError {
+	details: String,
+	kind: ZffErrorKind,
+}
+
+/// Contains the variants/kinds of errors, which could be find in this crate.
+#[derive(Debug, Clone)]
+pub enum ZffErrorKind {
+	IoError,
+	Custom,
+}
+
+impl fmt::Display for ZffErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let err_msg = match self {
+			ZffErrorKind::IoError => "IoError",
+			ZffErrorKind::Custom => "Custom",
+		};
+	write!(f, "{}", err_msg)
+	}
+}
+
+impl ZffError {
+	/// Creates a new crate-related error.
+	/// # Example
+	/// ```
+	/// use zff::{ZffError, ZffErrorKind, Result};
+	/// fn my_func() -> Result<()> {
+	/// 	let custom_error = ZffError::new(
+	///								ZffErrorKind::Custom, "My detailed custom error message");
+	///		Err(custom_error)
+	/// }
+	/// fn main() {
+	///		match my_func() {
+	///			Err(x) => println!("It work's! Your custom error message is: {}", x),
+	///			_ => ()
+	///		}
+	/// }
+	pub fn new<S: Into<String>>(kind: ZffErrorKind, details: S) -> ZffError {
+		ZffError {
+			kind: kind,
+			details: details.into()
+		}
+	}
+
+	pub fn new_custom<S: Into<String>>(details: S) -> ZffError {
+		ZffError {
+			kind: ZffErrorKind::Custom,
+			details: details.into()
+		}
+	}
+}
+
+impl From<io::Error> for ZffError {
+	fn from(e: io::Error) -> ZffError {
+		ZffError::new(ZffErrorKind::IoError, e.to_string())
+	}
+}
+
+impl fmt::Display for ZffError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let err_msg = format!("{}: {}", self.kind.to_string(), self.details);
+		write!(f, "{}", err_msg)
+	}
+}
