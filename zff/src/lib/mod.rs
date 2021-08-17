0e75674f9c3ebbff96445df5bf4e5ce0f5f420a7
ph0llux <ph0llux@pm.me>
2021-08-17 16:12:07 +0200

    custom Result type implemented

--- zff/src/lib/mod.rs
+++ zff/src/lib/mod.rs
@@ -1,9 +1,16 @@
 // - modules
+mod error;
 mod header;
 mod constants;
 mod traits;
+mod compression;
 
 // - re-exports
+pub use error::*;
 pub use header::*;
 pub use constants::*;
 pub use traits::*;
+pub use compression::*;
+
+// - types
+pub type Result<T> = std::result::Result<T, ZffError>;
