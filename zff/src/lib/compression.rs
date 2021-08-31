1570df7f53f1c30aa0fa8e725213ed29c379c5b8
ph0llux <ph0llux@pm.me>
2021-08-31 16:24:25 +0200

    method to write data to file

--- zff/src/lib/compression.rs
+++ zff/src/lib/compression.rs
@@ -1,5 +1,5 @@
 // - STD
-use std::io::Read;
+use std::io::{Read,Write};
 
 // - internal
 use crate::{
@@ -38,4 +38,39 @@ where
 			return Ok(Box::new(decoder));
 		}
 	}
+}
+
+pub fn write_from_compressed_filestream<R, W>(
+	input: &mut R,
+	output: &mut W,
+	split_size: usize,
+	buffersize: usize) -> Result<u64>
+where
+	R: Read + 'static,
+	W: Write,
+{
+	let mut buffer = vec![0; buffersize];
+	let mut written_bytes: usize = 0;
+	loop {
+		let mut bytes_read = 0;
+		while bytes_read < buffersize {
+			let r = input.read(&mut buffer[bytes_read..])?;
+			if r == 0 {
+				break;
+			}
+			bytes_read += r;
+		}
+		if bytes_read == 0 {
+			break;
+		};
+		output.write_all(&buffer)?;
+		written_bytes += bytes_read;
+		if bytes_read != buffersize {
+			break;
+		}
+		if written_bytes >= split_size {
+			break;
+		}
+	}
+	Ok(written_bytes as u64)
 }
