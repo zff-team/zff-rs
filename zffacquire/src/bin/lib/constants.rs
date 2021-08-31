50a46af428db767c8d700ac6c7438eb58e2709b8
ph0llux <ph0llux@pm.me>
2021-08-31 16:25:40 +0200

    full implementation of writing (splitted) output file (segments)

--- zffacquire/src/bin/lib/constants.rs
+++ zffacquire/src/bin/lib/constants.rs
@@ -47,19 +47,35 @@ pub const CLAP_ARG_SHORT_COMPRESSION_LEVEL: &str = "l";
 pub const CLAP_ARG_LONG_COMPRESSION_LEVEL: &str = "compression-level";
 pub const CLAP_ARG_POSSIBLE_VALUES_COMPRESSION_LEVEL: [&str; 9] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+pub const CLAP_ARG_NAME_SPLIT_SIZE: &str = "SPLIT_SIZE";
+pub const CLAP_ARG_HELP_SPLIT_SIZE: &str = "The split size of the output-file(s). Default is 0 (=the output image will never be splitted).";
+pub const CLAP_ARG_SHORT_SPLIT_SIZE: &str = "s";
+pub const CLAP_ARG_LONG_SPLIT_SIZE: &str = "split-size";
+
+pub const CLAP_ARG_NAME_SECTOR_SIZE: &str = "SECTOR_SIZE";
+pub const CLAP_ARG_HELP_SECTOR_SIZE: &str = "The sector size. Default is 512 bytes.";
+pub const CLAP_ARG_SHORT_SECTOR_SIZE: &str = "S";
+pub const CLAP_ARG_LONG_SECTOR_SIZE: &str = "sector-size";
+pub const CLAP_ARG_POSSIBLE_VALUES_SECTOR_SIZE: [&str; 8] = ["512", "1024", "2048", "4096", "8192", "16384", "32768", "65536"];
+
 // - header versions
 pub const MAIN_HEADER_VERSION: u8 = 1;
 pub const COMPRESSION_HEADER_VERSION: u8 = 1;
 pub const DESCRIPTION_HEADER_VERSION: u8 = 1;
+pub const SPLIT_HEADER_VERSION: u8 = 1;
 
 // compression
 pub const DEFAULT_COMPRESSION_LEVEL: u8 = 3;
 
 //Error messages
 pub const ERROR_OPEN_INPUT_FILE: &'static str = "Could not open input file: ";
-pub const ERROR_READ_METADATA_INPUT_FILE: &'static str = "Could not read metadata of input file: ";
 pub const ERROR_CREATE_OUTPUT_FILE: &'static str = "Could not create output file: ";
 pub const ERROR_WRITE_MAIN_HEADER: &'static str = "Could not write main header to file: ";
+pub const ERROR_WRITE_SPLIT_HEADER: &'static str = "Could not write split header to file: ";
 pub const ERROR_CREATE_COMPRESS_FILESTREAM: &'static str = "Could not create a compression stream around the input file. Maybe you want to try to dump uncompressed?";
 pub const ERROR_COPY_FILESTREAM_TO_OUTPUT: &'static str = "An I/O error occurred while trying to copy data from input to output file(s): ";
 pub const ERROR_REWRITE_MAIN_HEADER: &'static str = "An error occurred while trying to rewrite the main header to the output file. The written data length specified is not correctly listed in the header: ";
+pub const ERROR_PARSE_STR_SPLIT_SIZE: &'static str = "Could not value as valid split size: ";
+pub const ERROR_SET_FILE_EXTENSION: &'static str = "Could not set file extension for next part of image: ";
+
+pub const EXIT_STATUS_ERROR: i32 = 1;
--- zffacquire/src/bin/main.rs
+++ zffacquire/src/bin/main.rs
@@ -2,9 +2,9 @@
 use std::{
     time::{SystemTime,UNIX_EPOCH},
     path::{PathBuf},
-    fs::{File},
+    fs::{File, remove_file},
     process::exit,
-    io::{Write, Seek, SeekFrom, copy},
+    io::{Write, Seek, SeekFrom},
 };
 
 // - extern crates
@@ -20,8 +20,13 @@ use zff::{
     DescriptionHeader,
     CompressionHeader,
     CompressionAlgorithm,
+    SplitHeader,
     HeaderEncoder,
     compress_filestream,
+    write_from_compressed_filestream,
+    file_extension_next_value,
+    FILE_EXTENSION_FIRST_VALUE,
+    MINIMUM_SECTOR_SIZE,
 };
 
 // - external
@@ -30,6 +35,7 @@ use clap::{
     App,
     ArgMatches
 };
+use rand::Rng;
 
 fn arguments() -> ArgMatches<'static> {
     let matches = App::new(PROGRAM_NAME)
@@ -59,6 +65,17 @@ fn arguments() -> ArgMatches<'static> {
                         .short(CLAP_ARG_SHORT_COMPRESSION_LEVEL)
                         .long(CLAP_ARG_LONG_COMPRESSION_LEVEL)
                         .possible_values(&CLAP_ARG_POSSIBLE_VALUES_COMPRESSION_LEVEL)
+                        .takes_value(true))
+                    .arg(Arg::with_name(CLAP_ARG_NAME_SPLIT_SIZE)
+                        .help(CLAP_ARG_HELP_SPLIT_SIZE)
+                        .short(CLAP_ARG_SHORT_SPLIT_SIZE)
+                        .long(CLAP_ARG_LONG_SPLIT_SIZE)
+                        .takes_value(true))
+                    .arg(Arg::with_name(CLAP_ARG_NAME_SECTOR_SIZE)
+                        .help(CLAP_ARG_HELP_SECTOR_SIZE)
+                        .short(CLAP_ARG_SHORT_SECTOR_SIZE)
+                        .long(CLAP_ARG_LONG_SECTOR_SIZE)
+                        .possible_values(&CLAP_ARG_POSSIBLE_VALUES_SECTOR_SIZE)
                         .takes_value(true))
                     .arg(Arg::with_name(CLAP_ARG_NAME_CASE_NUMBER)
                         .help(CLAP_ARG_HELP_CASE_NUMBER)
@@ -117,42 +134,86 @@ fn description_header(arguments: &ArgMatches) -> DescriptionHeader {
     description_header
 }
 
-fn main() {
-	let arguments = arguments();
-    let compression_header = compression_header(&arguments);
-    let description_header = description_header(&arguments);
+fn split_header() -> SplitHeader {
+    let header_version = SPLIT_HEADER_VERSION;
+    let unique_identifier: u64 = rand::thread_rng().gen();
+    let split_number = 1;
+    let length_of_split = 0;
+    SplitHeader::new(header_version, unique_identifier, split_number, length_of_split)
+}
 
-    // Calling .unwrap() is safe here because the arguments are *required*.
-    let input_path = PathBuf::from(arguments.value_of(CLAP_ARG_NAME_INPUT_FILE).unwrap());
+fn calculate_split_size(arguments: &ArgMatches) -> u64 {
+    //TODO: support human readable split size entries, like 4G or 200M.
+    if let Some(value) = arguments.value_of(CLAP_ARG_NAME_SPLIT_SIZE) {
+        match value.parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("{}{}", ERROR_PARSE_STR_SPLIT_SIZE, value);
+                exit(EXIT_STATUS_ERROR);
+            }
+        }
+    } else {
+        0
+    }
+}
+
+fn calculate_sector_size(arguments: &ArgMatches) -> u8 {
+    if let Some(value) = arguments.value_of(CLAP_ARG_NAME_SECTOR_SIZE) {
+        // Calling .unwrap() is safe here because the possible values are limited
+        let value: u32 = value.parse().unwrap();
+        (value / 512) as u8
+    } else {
+        MINIMUM_SECTOR_SIZE as u8
+    }
+}
+
+fn write_to_output<O>(
+    input_path: &PathBuf,
+    output_filename: O,
+    mut main_header: MainHeader,
+    compression_header: CompressionHeader,
+    mut split_header: SplitHeader)
+where
+    O: Into<String>,
+{
     let input_file = match File::open(input_path) {
         Ok(file) => file,
         Err(e) => {
             println!("{}{}", ERROR_OPEN_INPUT_FILE, e.to_string());
-            exit(1);
+            exit(EXIT_STATUS_ERROR);
         },
     };
-    let output_filename = arguments.value_of(CLAP_ARG_NAME_OUTPUT_FILE).unwrap();
-    let mut main_header = MainHeader::new(MAIN_HEADER_VERSION, compression_header.clone(), description_header, 0);
-
-    let mut output_path = PathBuf::from(output_filename);
-    output_path.set_extension("z01"); //TODO
-
-    let mut output_file = match File::create(&output_path) {
-        Ok(file) => file,
-        Err(e) => {
-            println!("{}{}; {}", ERROR_CREATE_OUTPUT_FILE, output_path.to_string_lossy(), e.to_string());
-            exit(1);
-        }
+    let output_filename = output_filename.into();
+    let split_size = match main_header.split_size() {
+        0 => u64::MAX,
+        _ => main_header.split_size(),
     };
 
-    let mut filestream = match compress_filestream(
+    let mut input_file = match compress_filestream(
         input_file,
         compression_header.compression_algorithm(),
         compression_header.compression_level()) {
         Ok(stream) => stream,
         Err(_) => {
             println!("{}", ERROR_CREATE_COMPRESS_FILESTREAM);
-            exit(1);
+            exit(EXIT_STATUS_ERROR);
+        }
+    };
+
+    let header_size = main_header.get_encoded_size();
+
+    let sector_size = main_header.sector_size();
+
+    let first_segment_size = split_size as usize - header_size;
+    let mut first_segment_filename = PathBuf::from(&output_filename);
+    let mut file_extension = String::from(FILE_EXTENSION_FIRST_VALUE);
+    first_segment_filename.set_extension(&file_extension);
+
+    let mut output_file = match File::create(&first_segment_filename) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("{}{}", ERROR_CREATE_OUTPUT_FILE, &first_segment_filename.to_string_lossy());
+            exit(EXIT_STATUS_ERROR);
         }
     };
 
@@ -160,16 +221,71 @@ fn main() {
         Ok(_) => (),
         Err(e) => {
             println!("{}{}", ERROR_WRITE_MAIN_HEADER, e.to_string());
-            exit(1);
+            exit(EXIT_STATUS_ERROR);
         }
     };
-    let written_bytes = match copy(&mut filestream, &mut output_file) {
-        Ok(bytes) => bytes,
+    
+    let mut written_bytes = match write_from_compressed_filestream(
+        &mut input_file,
+        &mut output_file,
+        first_segment_size,
+        sector_size) {
+        Ok(val) => val,
         Err(e) => {
-            println!("{}{}", ERROR_CREATE_COMPRESS_FILESTREAM, e.to_string());
-            exit(1);
-        }
+            println!("{}{}", ERROR_COPY_FILESTREAM_TO_OUTPUT, e.to_string());
+            exit(EXIT_STATUS_ERROR);
+        },
     };
+
+    split_header.set_length_of_split(written_bytes);
+    main_header.set_split_header(split_header.clone());
+
+    loop {
+        let segment_split_header = split_header.next_header();
+        file_extension = match file_extension_next_value(&file_extension) {
+            Ok(val) => val,
+            Err(e) => {
+                println!("{}{}", ERROR_SET_FILE_EXTENSION, e.to_string());
+                exit(EXIT_STATUS_ERROR);
+            }
+        };
+        let mut segment_filename = PathBuf::from(&output_filename);
+        segment_filename.set_extension(&file_extension);
+        let mut output_file = match File::create(&segment_filename) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("{}{}", ERROR_CREATE_OUTPUT_FILE, &segment_filename.to_string_lossy());
+                exit(EXIT_STATUS_ERROR);
+            }
+        };
+
+        match output_file.write(&segment_split_header.encode_directly()) {
+            Ok(_) => (),
+            Err(_) => {
+                println!("{}{}", ERROR_WRITE_SPLIT_HEADER, segment_filename.to_string_lossy());
+                exit(EXIT_STATUS_ERROR);
+            }
+        };
+
+        let written_bytes_in_segment = match write_from_compressed_filestream(
+            &mut input_file,
+            &mut output_file,
+            split_size as usize,
+            sector_size) {
+
+            Ok(val) => val,
+            Err(e) => {
+                println!("{}{}", ERROR_COPY_FILESTREAM_TO_OUTPUT, e.to_string());
+                exit(EXIT_STATUS_ERROR);
+            }
+        };
+        if written_bytes_in_segment == 0 {
+            let _ = remove_file(segment_filename);
+            break;
+        } else {
+            written_bytes += written_bytes_in_segment
+        }
+    }
 
     //rewrite main_header with the correct number of bytes of the COMPRESSED data.
     main_header.set_length_of_data(written_bytes);
@@ -177,14 +293,38 @@ fn main() {
         Ok(_) => (),
         Err(e) => {
             println!("{}{}", ERROR_REWRITE_MAIN_HEADER, e.to_string());
-            exit(1);
+            exit(EXIT_STATUS_ERROR);
         }
     }
     match output_file.write(&main_header.encode_directly()) {
         Ok(_) => (),
         Err(e) => {
             println!("{}{}", ERROR_REWRITE_MAIN_HEADER, e.to_string());
-            exit(1);
+            exit(EXIT_STATUS_ERROR);
         }
     };
+}
+
+fn main() {
+	let arguments = arguments();
+    let compression_header = compression_header(&arguments);
+    let description_header = description_header(&arguments);
+    let split_size = calculate_split_size(&arguments);
+    let sector_size = calculate_sector_size(&arguments);
+    let split_header = split_header();
+
+    // Calling .unwrap() is safe here because the arguments are *required*.
+    let input_path = PathBuf::from(arguments.value_of(CLAP_ARG_NAME_INPUT_FILE).unwrap());
+    let output_filename = arguments.value_of(CLAP_ARG_NAME_OUTPUT_FILE).unwrap();
+
+    let main_header = MainHeader::new(
+        MAIN_HEADER_VERSION,
+        compression_header.clone(),
+        description_header,
+        sector_size,
+        split_size,
+        split_header.clone(),
+        0);
+
+    write_to_output(&input_path, output_filename, main_header, compression_header, split_header);
 }
