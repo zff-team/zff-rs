bd15c99b9d4645d0651947f354366c9fc97f0044
ph0llux <ph0llux@pm.me>
2021-09-03 00:37:08 +0200

    chunk header added

---
+++ zff/src/lib/header/chunk_header.rs
@@ -0,0 +1,73 @@
+// - internal
+use crate::{
+	HeaderEncoder,
+	HeaderObject,
+	ValueType,
+	HEADER_IDENTIFIER_CHUNK_HEADER,
+};
+
+#[derive(Debug,Clone)]
+pub struct ChunkHeader {
+	header_version: u8,
+	chunk_number: u64,
+	chunk_size: u64
+}
+
+impl ChunkHeader {
+	pub fn new(header_version: u8, chunk_number: u64, chunk_size: u64) -> ChunkHeader {
+		Self {
+			header_version: header_version,
+			chunk_number: chunk_number,
+			chunk_size: chunk_size,
+		}
+	}
+
+	pub fn set_chunk_size(&mut self, size: u64) {
+		self.chunk_size = size
+	}
+
+	pub fn next_header(&mut self) -> ChunkHeader {
+		Self {
+			header_version: self.header_version,
+			chunk_number: self.chunk_number+1,
+			chunk_size: 0,
+		}
+	}
+}
+
+impl HeaderObject for ChunkHeader {
+	fn identifier() -> u32 {
+		HEADER_IDENTIFIER_CHUNK_HEADER
+	}
+	fn encode_header(&self) -> Vec<u8> {
+		let mut vec = Vec::new();
+
+		vec.push(self.header_version);
+		vec.append(&mut self.chunk_number.encode_directly());
+		vec.append(&mut self.chunk_size.encode_directly());
+
+		vec
+	}
+}
+
+impl HeaderEncoder for ChunkHeader {
+	fn encode_directly(&self) -> Vec<u8> {
+		let mut vec = Vec::new();
+		let mut encoded_header = self.encode_header();
+		let identifier = Self::identifier();
+		let encoded_header_length = 4 + 8 + (encoded_header.len() as u64); //4 bytes identifier + 8 bytes for length + length itself
+		vec.append(&mut identifier.to_be_bytes().to_vec());
+		vec.append(&mut encoded_header_length.to_le_bytes().to_vec());
+		vec.append(&mut encoded_header);
+
+		vec
+	}
+	fn encode_for_key<K: Into<String>>(&self, key: K) -> Vec<u8> {
+		let mut vec = Vec::new();
+		let mut encoded_key = Self::encode_key(key);
+		vec.append(&mut encoded_key);
+		vec.push(ValueType::Object.clone() as u8);
+		vec.append(&mut self.encode_directly());
+		vec
+	}
+}
--- zff/src/lib/header/mod.rs
+++ zff/src/lib/header/mod.rs
@@ -6,6 +6,7 @@ mod encryption_header;
 mod description_header;
 mod split_header;
 mod compression_header;
+mod chunk_header;
 
 // - re-exports
 pub use coding::*;
@@ -15,3 +16,4 @@ pub use encryption_header::*;
 pub use description_header::*;
 pub use split_header::*;
 pub use compression_header::*;
+pub use chunk_header::*;
