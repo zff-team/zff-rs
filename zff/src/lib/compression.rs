68f51fbed45bcb6a54f0b6bdeec3ecbf2c17e201
ph0llux <ph0llux@pm.me>
2021-09-03 00:39:46 +0200

    support for dump as chunked data added

--- zff/src/lib/compression.rs
+++ zff/src/lib/compression.rs
@@ -4,7 +4,11 @@ use std::io::{Read,Write};
 // - internal
 use crate::{
 	Result,
+	ChunkHeader,
 	CompressionAlgorithm,
+	HeaderEncoder,
+	ZffError,
+	ZffErrorKind,
 };
 
 // - external
@@ -40,37 +44,122 @@ where
 	}
 }
 
-pub fn write_from_compressed_filestream<R, W>(
-	input: &mut R,
-	output: &mut W,
-	split_size: usize,
-	buffersize: usize) -> Result<u64>
+pub fn compress_filestream_writer<W>(output: W, algorithm: CompressionAlgorithm, compression_level: &u8) -> Result<Box<dyn Write>>
 where
-	R: Read + 'static,
-	W: Write,
+	W: Write + 'static
 {
-	let mut buffer = vec![0; buffersize];
-	let mut written_bytes: usize = 0;
-	loop {
-		let mut bytes_read = 0;
-		while bytes_read < buffersize {
-			let r = input.read(&mut buffer[bytes_read..])?;
-			if r == 0 {
-				break;
-			}
-			bytes_read += r;
-		}
-		if bytes_read == 0 {
-			break;
-		};
-		output.write_all(&buffer)?;
-		written_bytes += bytes_read;
-		if bytes_read != buffersize {
-			break;
-		}
-		if written_bytes >= split_size {
-			break;
+	match algorithm {
+		CompressionAlgorithm::None => {
+			return Ok(Box::new(output))
+		},
+		CompressionAlgorithm::Zstd => {
+			let encoder = zstd::stream::write::Encoder::new(output, *compression_level as i32)?.auto_finish();
+			return Ok(Box::new(encoder))
 		}
 	}
+}
+
+fn buffer_chunk<R>(
+	input: &mut R,
+	chunk_size: usize,
+	) -> Result<Vec<u8>>
+where
+	R: Read
+{
+	let mut buf = vec![0u8; chunk_size];
+    let mut bytes_read = 0;
+
+    while bytes_read < chunk_size {
+        let r = input.read(&mut buf[bytes_read..])?;
+        if r == 0 {
+            break;
+        }
+        bytes_read += r;
+    }
+
+    let buf = if bytes_read == chunk_size {
+        buf
+    } else {
+        buf[..bytes_read].to_vec()
+    };
+    return Ok(buf)
+}
+
+fn source_read_chunk<R>(
+	input: &mut R,
+	chunk_size: usize,
+	algorithm: &CompressionAlgorithm,
+	compression_level: &u8) -> Result<Vec<u8>>
+where
+    R: Read
+{
+    let buf = buffer_chunk(input, chunk_size)?;
+    if buf.len() == 0 {
+    	return Err(ZffError::new(ZffErrorKind::ReadEOF, ""));
+    };
+
+    match algorithm {
+    	CompressionAlgorithm::None => return Ok(buf),
+    	CompressionAlgorithm::Zstd => {
+    		let mut stream = zstd::stream::read::Encoder::new(buf.as_slice(), *compression_level as i32)?;
+    		let buf = buffer_chunk(&mut stream, chunk_size*4)?;
+    		Ok(buf)
+    	}
+    }
+}
+
+
+fn write_unencrypted_chunk<R, W>(
+	input: &mut R,
+	output: &mut W,
+	chunk_size: usize,
+	mut chunk_header: ChunkHeader,
+	compression_algorithm: &CompressionAlgorithm,
+	compression_level: &u8) -> Result<u64>
+where
+	R: Read,
+	W: Write
+{
+	let compressed_chunk = source_read_chunk(input, chunk_size, compression_algorithm, compression_level)?;
+	chunk_header.set_chunk_size(compressed_chunk.len() as u64);
+
+	let mut written_bytes = 0;
+	written_bytes += output.write(&chunk_header.encode_directly())?;
+	written_bytes += output.write(&compressed_chunk)?;
 	Ok(written_bytes as u64)
+}
+
+pub fn write_segment<R, W>(
+	input: &mut R,
+	output: &mut W,
+	chunk_size: usize,
+	mut chunk_header: ChunkHeader,
+	compression_algorithm: &CompressionAlgorithm,
+	compression_level: &u8,
+	split_size: usize) -> Result<u64>
+where
+	R: Read,
+	W: Write
+{
+	let mut written_bytes: u64 = 0;
+	loop {
+		if (written_bytes + chunk_size as u64) > split_size as u64 {
+			return Ok(written_bytes);
+		};
+		let chunk_header = chunk_header.next_header();
+		let written_in_chunk = match write_unencrypted_chunk(
+			input,
+			output,
+			chunk_size,
+			chunk_header,
+			compression_algorithm,
+			compression_level) {
+			Ok(data) => data,
+			Err(e) => match e.get_kind() {
+				ZffErrorKind::ReadEOF => return Ok(written_bytes),
+				_ => return Err(e),
+			},
+		};
+		written_bytes += written_in_chunk;
+	}
 }
