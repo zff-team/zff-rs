188153e340bf7286f0f7d688a69124707c45c34e
ph0llux <ph0llux@pm.me>
2021-09-03 00:36:47 +0200

    header encryption support added

--- zff/src/lib/header/main_header.rs
+++ zff/src/lib/header/main_header.rs
@@ -8,16 +8,20 @@ use crate::{
 	DescriptionHeader,
 	EncryptionHeader,
 	SplitHeader,
+	ZffError,
+	ZffErrorKind,
+	Encryption,
 	HEADER_IDENTIFIER_MAIN_HEADER,
-	MINIMUM_SECTOR_SIZE,
+	HEADER_IDENTIFIER_ENCRYPTED_MAIN_HEADER,
 };
 
 #[derive(Debug,Clone)]
 pub struct MainHeader {
 	header_version: u8,
+	encryption_header: Option<EncryptionHeader>,
 	compression_header: CompressionHeader,
 	description_header: DescriptionHeader,
-	sector_size: u8,
+	chunk_size: u8,
 	split_size_in_bytes: u64,
 	split_header: SplitHeader,
 	length_of_data: u64,
@@ -26,21 +30,72 @@ pub struct MainHeader {
 impl MainHeader {
 	pub fn new(
 		header_version: u8,
+		encryption_header: Option<EncryptionHeader>,
 		compression_header: CompressionHeader,
 		description_header: DescriptionHeader,
-		sector_size: u8,
+		chunk_size: u8,
 		split_size_in_bytes: u64,
 		split_header: SplitHeader,
 		length_of_data: u64) -> MainHeader {
 		Self {
 			header_version: header_version,
+			encryption_header: encryption_header,
 			compression_header: compression_header,
 			description_header: description_header,
-			sector_size: sector_size,
+			chunk_size: chunk_size,
 			split_size_in_bytes: split_size_in_bytes,
 			split_header: split_header,
 			length_of_data: length_of_data,
 		}
+	}
+
+	fn encode_encrypted_header<K>(&self, key: K) -> Result<Vec<u8>>
+	where
+		K: AsRef<[u8]>
+	{
+		let mut vec = Vec::new();
+		vec.push(self.header_version);
+		let encryption_header = match &self.encryption_header {
+			None => return Err(ZffError::new(ZffErrorKind::MissingEncryptionHeader, "")),
+			Some(header) => {
+				header
+			}
+		};
+		let encryption_flag: u8 = 1;
+		vec.push(encryption_flag);
+		vec.append(&mut encryption_header.encode_directly());
+
+		let data_to_encrypt = Vec::new();
+		vec.append(&mut self.compression_header.encode_directly());
+		vec.append(&mut self.description_header.encode_directly());
+		vec.push(self.chunk_size);
+		vec.append(&mut self.split_size_in_bytes.encode_directly());
+		vec.append(&mut self.split_header.encode_directly());
+		vec.append(&mut self.length_of_data.encode_directly());
+
+		let mut encrypted_data = Encryption::encrypt_header(
+			key, data_to_encrypt,
+			encryption_header.encryption_key_nonce(),
+			encryption_header.encryption_algorithm()
+			)?;
+
+		vec.append(&mut encrypted_data);
+		return Ok(vec);
+	}
+
+	pub fn encode_encrypted_header_directly<K>(&self, key: K) -> Result<Vec<u8>>
+	where
+		K: AsRef<[u8]>,
+	{
+		let mut vec = Vec::new();
+		let mut encoded_header = self.encode_encrypted_header(key)?;
+		let identifier = HEADER_IDENTIFIER_ENCRYPTED_MAIN_HEADER;
+		let encoded_header_length = 4 + 8 + (encoded_header.len() as u64); //4 bytes identifier + 8 bytes for length + length itself
+		vec.append(&mut identifier.to_be_bytes().to_vec());
+		vec.append(&mut encoded_header_length.to_le_bytes().to_vec());
+		vec.append(&mut encoded_header);
+
+		Ok(vec)
 	}
 
 	pub fn new_from_encrypted_header(
@@ -63,8 +118,8 @@ impl MainHeader {
 		self.header_version
 	}
 
-	pub fn sector_size(&self) -> usize {
-		MINIMUM_SECTOR_SIZE * (1<<self.sector_size)
+	pub fn chunk_size(&self) -> usize {
+		1<<self.chunk_size
 	}
 
 	pub fn split_size(&self) -> u64 {
@@ -84,9 +139,20 @@ impl HeaderObject for MainHeader {
 		let mut vec = Vec::new();
 
 		vec.push(self.header_version);
+		match &self.encryption_header {
+			None => {
+				let encryption_flag: u8 = 0;
+				vec.push(encryption_flag);
+			},
+			Some(header) => {
+				let encryption_flag: u8 = 1;
+				vec.push(encryption_flag);
+				vec.append(&mut header.encode_directly());
+			},
+		};
 		vec.append(&mut self.compression_header.encode_directly());
 		vec.append(&mut self.description_header.encode_directly());
-		vec.push(self.sector_size);
+		vec.push(self.chunk_size);
 		vec.append(&mut self.split_size_in_bytes.encode_directly());
 		vec.append(&mut self.split_header.encode_directly());
 		vec.append(&mut self.length_of_data.encode_directly());
