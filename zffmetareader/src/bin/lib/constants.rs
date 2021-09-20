133fbd779e1ef0cca684a212c10e79b7698f60e8
ph0llux <ph0llux@pm.me>
2021-09-21 00:18:03 +0200

    toml,json,json_pretty output added

--- zffmetareader/src/bin/lib/constants.rs
+++ zffmetareader/src/bin/lib/constants.rs
@@ -9,3 +9,23 @@ pub const CLAP_ARG_NAME_INPUT_FILE: &str = "INPUT_FILE";
 pub const CLAP_ARG_HELP_INPUT_FILE: &str = "The input file. This should be your device to dump.";
 pub const CLAP_ARG_SHORT_INPUT_FILE: &str = "i";
 pub const CLAP_ARG_LONG_INPUT_FILE: &str = "inputfile";
+
+// - args
+pub const CLAP_ARG_NAME_OUTPUT_FORMAT: &str = "OUTPUT_FORMAT";
+pub const CLAP_ARG_HELP_OUTPUT_FORMAT: &str = "The output format.";
+pub const CLAP_ARG_SHORT_OUTPUT_FORMAT: &str = "f";
+pub const CLAP_ARG_LONG_OUTPUT_FORMAT: &str = "output-format";
+pub const CLAP_ARG_POSSIBLE_VALUES_OUTPUT_FORMAT: [&str; 3] = [CLAP_ARG_VALUE_OUTPUT_FORMAT_TOML, CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON, CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON_PRETTY];
+pub const CLAP_ARG_DEFAULT_VALUE_OUTPUT_FORMAT: &'static str = CLAP_ARG_VALUE_OUTPUT_FORMAT_TOML;
+pub const CLAP_ARG_VALUE_OUTPUT_FORMAT_TOML: &'static str = "toml";
+pub const CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON: &'static str = "json";
+pub const CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON_PRETTY: &'static str = "json_pretty";
+
+// Error messages
+pub const ERROR_OPEN_INPUT_FILE: &str = "An errror occurred while trying to open the input file.";
+pub const ERROR_SERIALIZE_TOML: &str = "An errror occurred while trying to serialize the decoded information to toml format.";
+pub const ERROR_SERIALIZE_JSON: &str = "An errror occurred while trying to serialize the decoded information to json format.";
+pub const ERROR_SERIALIZE_UNKNOWN_SERIALIZER: &str = "Unknown output format.";
+
+pub const EXIT_STATUS_ERROR: i32 = 1;
+pub const EXIT_STATUS_SUCCESS: i32 = 0;
--- zffmetareader/src/bin/main.rs
+++ zffmetareader/src/bin/main.rs
@@ -1,11 +1,23 @@
+// - STD
+use std::fs::File;
+use std::path::PathBuf;
+use std::process::exit;
+
 // - modules
 mod lib;
 
 // - internal
 use lib::constants::*;
+use zff::{
+    header::*,
+    HeaderDecoder,
+    ZffErrorKind,
+};
 
 // - external
 use clap::{Arg, App, ArgMatches};
+use toml;
+use serde_json;
 
 fn arguments() -> ArgMatches<'static> {
     let matches = App::new(PROGRAM_NAME)
@@ -18,12 +30,80 @@ fn arguments() -> ArgMatches<'static> {
                         .long(CLAP_ARG_LONG_INPUT_FILE)
                         .required(true)
                         .takes_value(true))
-
+                    .arg(Arg::with_name(CLAP_ARG_NAME_OUTPUT_FORMAT)
+                        .help(CLAP_ARG_HELP_OUTPUT_FORMAT)
+                        .short(CLAP_ARG_SHORT_OUTPUT_FORMAT)
+                        .long(CLAP_ARG_LONG_OUTPUT_FORMAT)
+                        .possible_values(&CLAP_ARG_POSSIBLE_VALUES_OUTPUT_FORMAT)
+                        .default_value(CLAP_ARG_DEFAULT_VALUE_OUTPUT_FORMAT)
+                        .takes_value(true))
                     .get_matches();
     matches
 }
 
-pub fn main() {
+fn main() {
+    let arguments = arguments();
+
+    // Calling .unwrap() is safe here because the arguments are *required*.
+    let input_path = PathBuf::from(arguments.value_of(CLAP_ARG_NAME_INPUT_FILE).unwrap());
+    let mut input_file = match File::open(input_path) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("{}", ERROR_OPEN_INPUT_FILE);
+            exit(EXIT_STATUS_ERROR);
+        }
+    };
+
+    match MainHeader::decode_directly(&mut input_file) {
+        Ok(header) => {
+            // Calling .unwrap() is safe here because this argument has a default value.
+            match arguments.value_of(CLAP_ARG_NAME_OUTPUT_FORMAT).unwrap() {
+                CLAP_ARG_VALUE_OUTPUT_FORMAT_TOML => match toml::Value::try_from(&header) {
+                    Ok(value) => {
+                        println!("{}", value);
+                        exit(EXIT_STATUS_SUCCESS);
+                    },
+                    Err(_) => {
+                        println!("{}", ERROR_SERIALIZE_TOML);
+                        exit(EXIT_STATUS_ERROR);
+                    }
+                },
+                CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON => match serde_json::to_string(&header) {
+                    Ok(value) => {
+                        println!("{}", value);
+                        exit(EXIT_STATUS_SUCCESS);
+                    },
+                    Err(_) => {
+                        println!("{}", ERROR_SERIALIZE_JSON);
+                        exit(EXIT_STATUS_ERROR);
+                    }
+                },
+                CLAP_ARG_VALUE_OUTPUT_FORMAT_JSON_PRETTY => match serde_json::to_string_pretty(&header) {
+                    Ok(value) => {
+                        println!("{}", value);
+                        exit(EXIT_STATUS_SUCCESS);
+                    },
+                    Err(_) => {
+                        println!("{}", ERROR_SERIALIZE_JSON);
+                        exit(EXIT_STATUS_ERROR);
+                    }
+                }
+                _ => {
+                    println!("{}", ERROR_SERIALIZE_UNKNOWN_SERIALIZER);
+                    exit(EXIT_STATUS_ERROR);
+                }
+            }
+        },
+        Err(e) => match e.get_kind() {
+            ZffErrorKind::HeaderDecodeEncryptedMainHeader => {
+                println!("{}", e.to_string());
+                exit(EXIT_STATUS_ERROR);
+            }
+            _ => ()
+        }
+    }
+
+    //if this is a segment
     unimplemented!()
-    
+
 }
