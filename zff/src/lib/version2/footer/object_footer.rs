0dd668527ac9d6c25b66a88eb9ddf50bf7f277aa
ph0llux <ph0llux@pm.me>
2021-12-27 21:27:26 +0100

    length_of_data, first_chunk_number and number_of_chunk fields added

--- zff/src/lib/version2/footer/object_footer.rs
+++ zff/src/lib/version2/footer/object_footer.rs
@@ -6,6 +6,7 @@ use crate::{
 	Result,
 	HeaderCoding,
 	ValueDecoder,
+	ValueEncoder,
 	FOOTER_IDENTIFIER_OBJECT_FOOTER_PHYSICAL,
 	FOOTER_IDENTIFIER_OBJECT_FOOTER_LOGICAL,
 };
@@ -16,13 +17,19 @@ use crate::version2::header::{
 #[derive(Debug,Clone)]
 pub struct ObjectFooterPhysical {
 	version: u8,
+	length_of_data: u64,
+	first_chunk_number: u64,
+	number_of_chunks: u64,
 	hash_header: HashHeader,
 }
 
 impl ObjectFooterPhysical {
-	pub fn new(version: u8, hash_header: HashHeader) -> ObjectFooterPhysical {
+	pub fn new(version: u8, length_of_data: u64, first_chunk_number: u64, number_of_chunks: u64, hash_header: HashHeader) -> ObjectFooterPhysical {
 		Self {
 			version: version,
+			length_of_data: length_of_data,
+			first_chunk_number: first_chunk_number,
+			number_of_chunks: number_of_chunks,
 			hash_header: hash_header,
 		}
 	}
@@ -39,14 +46,20 @@ impl HeaderCoding for ObjectFooterPhysical {
 	fn encode_header(&self) -> Vec<u8> {
 		let mut vec = Vec::new();
 		vec.push(self.version);
+		vec.append(&mut self.length_of_data.encode_directly());
+		vec.append(&mut self.first_chunk_number.encode_directly());
+		vec.append(&mut self.number_of_chunks.encode_directly());
 		vec.append(&mut self.hash_header.encode_directly());
 		vec
 	}
 	fn decode_content(data: Vec<u8>) -> Result<ObjectFooterPhysical> {
 		let mut cursor = Cursor::new(data);
 		let footer_version = u8::decode_directly(&mut cursor)?;
+		let length_of_data = u64::decode_directly(&mut cursor)?;
+		let first_chunk_number = u64::decode_directly(&mut cursor)?;
+		let number_of_chunks = u64::decode_directly(&mut cursor)?;
 		let hash_header = HashHeader::decode_directly(&mut cursor)?;
-		Ok(ObjectFooterPhysical::new(footer_version, hash_header))
+		Ok(ObjectFooterPhysical::new(footer_version, length_of_data, first_chunk_number, number_of_chunks, hash_header))
 	}
 }
 
