455211365c16d94586f3b5929687a16c012dbc23
ph0llux <ph0llux@pm.me>
2021-12-28 16:03:00 +0100

    various default values moved

--- zff/src/lib/version1/constants.rs
+++ zff/src/lib/version1/constants.rs
@@ -1,11 +1,5 @@
 pub(crate) use crate::constants::*;
 
-//ZFF File extension
-/// the start value for file extensions. a file-extension always starts with a 'z', followed by the segment number (e.g. "z01", "z02", ..., "z99", "z100", ...).
-pub const FILE_EXTENSION_START: char = 'z';
-/// the file extension for the first segment (which contains the main header also).
-pub const FILE_EXTENSION_FIRST_VALUE: &str = "z01";
-
 pub(crate) const DEFAULT_LENGTH_SEGMENT_FOOTER_EMPTY: usize = 21;
 pub(crate) const DEFAULT_SEGMENT_FOOTER_VERSION: u8 = 1;
 
