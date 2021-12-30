9173f5254271842109f032bd43afe126f905e506
ph0llux <ph0llux@pm.me>
2021-12-30 21:16:22 +0100

    moved some constants

--- src/lib/version1/constants.rs
+++ src/lib/version1/constants.rs
@@ -6,9 +6,5 @@ pub(crate) const DEFAULT_SEGMENT_FOOTER_VERSION: u8 = 1;
 // default header versions.
 /// current header version for the [MainHeader](crate::header::MainHeader).
 pub const DEFAULT_HEADER_VERSION_MAIN_HEADER: u8 = 1;
-/// current header version for the [CompressionHeader](crate::header::CompressionHeader).
-pub const DEFAULT_HEADER_VERSION_COMPRESSION_HEADER: u8 = 1;
-/// current header version for the [DescriptionHeader](crate::header::DescriptionHeader).
-pub const DEFAULT_HEADER_VERSION_DESCRIPTION_HEADER: u8 = 1;
 /// current header version for the [SegmentHeader](crate::header::SegmentHeader).
 pub const DEFAULT_HEADER_VERSION_SEGMENT_HEADER: u8 = 1;
