6a89f913299ae28994381517c825fa8638673dff
ph0llux <ph0llux@pm.me>
2022-02-06 23:59:15 +0100

    implemented ObjectFooter-enum; added file_header_offset-table to ObjectFooterLogical-struct, following the specification for ZffV2

--- src/lib/version2/footer/object_footer.rs
+++ src/lib/version2/footer/object_footer.rs
@@ -1,5 +1,5 @@
 // - STD
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 use std::collections::HashMap;
 
 // - internal
@@ -8,12 +8,80 @@ use crate::{
 	HeaderCoding,
 	ValueDecoder,
 	ValueEncoder,
+	ZffError,
+	ZffErrorKind,
 	FOOTER_IDENTIFIER_OBJECT_FOOTER_PHYSICAL,
 	FOOTER_IDENTIFIER_OBJECT_FOOTER_LOGICAL,
+	DEFAULT_LENGTH_VALUE_HEADER_LENGTH,
+	DEFAULT_LENGTH_HEADER_IDENTIFIER,
+	ERROR_HEADER_DECODER_MISMATCH_IDENTIFIER,
+	ERROR_HEADER_DECODER_HEADER_LENGTH,
 };
 use crate::version2::header::{
 	HashHeader,
 };
+
+// - external
+use byteorder::{LittleEndian, BigEndian, ReadBytesExt};
+
+
+#[derive(Debug, Clone)]
+pub enum ObjectFooter {
+	Physical(ObjectFooterPhysical),
+	Logical(ObjectFooterLogical),
+}
+
+impl ObjectFooter {
+
+	pub fn version(&self) -> u8 {
+		match self {
+			ObjectFooter::Physical(phy) => phy.version(),
+			ObjectFooter::Logical(log) => log.version(),
+		}
+	}
+
+	/// checks if the identifier matches to an physical or logical object footer. Returns 1 for a physical object footer, 2 for a logical object footer and 0 if neither applies.
+	fn check_identifier<R: Read>(data: &mut R) -> u8 {
+		let identifier = match data.read_u32::<BigEndian>() {
+			Ok(val) => val,
+			Err(_) => return 0,
+		};
+		if identifier == ObjectFooterPhysical::identifier() { 
+			return 1;
+		} else if identifier == ObjectFooterLogical::identifier() {
+			return 2;
+		} else {
+			return 0;
+		}
+	}
+
+	/// decodes the length of the header.
+	fn decode_header_length<R: Read>(data: &mut R) -> Result<u64> {
+		match data.read_u64::<LittleEndian>() {
+			Ok(value) => Ok(value),
+			Err(_) => Err(ZffError::new_header_decode_error(ERROR_HEADER_DECODER_HEADER_LENGTH)),
+		}
+	}
+
+	/// decodes the header directly.
+	pub fn decode_directly<R: Read>(data: &mut R) -> Result<ObjectFooter> {
+		match Self::check_identifier(data) {
+			1 => {
+				let length = Self::decode_header_length(data)? as usize;
+				let mut content_buffer = vec![0u8; length-DEFAULT_LENGTH_HEADER_IDENTIFIER-DEFAULT_LENGTH_VALUE_HEADER_LENGTH];
+				data.read_exact(&mut content_buffer)?;
+				return Ok(ObjectFooter::Physical(ObjectFooterPhysical::decode_content(content_buffer)?));
+			},
+			2 => {
+				let length = Self::decode_header_length(data)? as usize;
+				let mut content_buffer = vec![0u8; length-DEFAULT_LENGTH_HEADER_IDENTIFIER-DEFAULT_LENGTH_VALUE_HEADER_LENGTH];
+				data.read_exact(&mut content_buffer)?;
+				return Ok(ObjectFooter::Logical(ObjectFooterLogical::decode_content(content_buffer)?));
+			},
+			_ => return Err(ZffError::new(ZffErrorKind::HeaderDecodeMismatchIdentifier, ERROR_HEADER_DECODER_MISMATCH_IDENTIFIER)),
+		}
+	}
+}
 
 #[derive(Debug,Clone)]
 pub struct ObjectFooterPhysical {
@@ -37,6 +105,14 @@ impl ObjectFooterPhysical {
 			number_of_chunks: number_of_chunks,
 			hash_header: hash_header,
 		}
+	}
+
+	pub fn first_chunk_number(&self) -> u64 {
+		self.first_chunk_number
+	}
+
+	pub fn length_of_data(&self) -> u64 {
+		self.length_of_data
 	}
 }
 
@@ -75,6 +151,8 @@ impl HeaderCoding for ObjectFooterPhysical {
 #[derive(Debug,Clone)]
 pub struct ObjectFooterLogical {
 	version: u8,
+	file_header_segment_numbers: HashMap<u64, u64>,
+	file_header_offsets: HashMap<u64, u64>,
 	file_footer_segment_numbers: HashMap<u64, u64>,
 	file_footer_offsets: HashMap<u64, u64>,
 }
@@ -83,24 +161,53 @@ impl ObjectFooterLogical {
 	pub fn new_empty(version: u8) -> ObjectFooterLogical {
 		Self {
 			version: version,
+			file_header_segment_numbers: HashMap::new(),
+			file_header_offsets: HashMap::new(),
 			file_footer_segment_numbers: HashMap::new(),
 			file_footer_offsets: HashMap::new()
 		}
 	}
-	pub fn new(version: u8, file_footer_segment_numbers: HashMap<u64, u64>, file_footer_offsets: HashMap<u64, u64>) -> ObjectFooterLogical {
+	pub fn new(
+		version: u8,
+		file_header_segment_numbers: HashMap<u64, u64>,
+		file_header_offsets: HashMap<u64, u64>,
+		file_footer_segment_numbers: HashMap<u64, u64>,
+		file_footer_offsets: HashMap<u64, u64>) -> ObjectFooterLogical {
 		Self {
 			version: version,
+			file_header_segment_numbers: file_header_segment_numbers,
+			file_header_offsets: file_header_offsets,
 			file_footer_segment_numbers: file_footer_segment_numbers,
 			file_footer_offsets: file_footer_offsets,
 		}
 	}
 
-	pub fn add_file_segment_number(&mut self, filenumber: u64, file_segment_number: u64) {
+	pub fn add_file_header_segment_number(&mut self, filenumber: u64, file_segment_number: u64) {
+		self.file_header_segment_numbers.insert(filenumber, file_segment_number);
+	}
+
+	pub fn add_file_header_offset(&mut self, filenumber: u64, fileoffset: u64) {
+		self.file_header_offsets.insert(filenumber, fileoffset);
+	}
+
+	pub fn file_header_segment_numbers(&self) -> &HashMap<u64, u64> {
+		&self.file_header_segment_numbers
+	}
+
+	pub fn file_header_offsets(&self) -> &HashMap<u64, u64> {
+		&self.file_header_offsets
+	}
+
+	pub fn add_file_footer_segment_number(&mut self, filenumber: u64, file_segment_number: u64) {
 		self.file_footer_segment_numbers.insert(filenumber, file_segment_number);
 	}
 
-	pub fn add_fileoffset(&mut self, filenumber: u64, fileoffset: u64) {
+	pub fn add_file_footer_offset(&mut self, filenumber: u64, fileoffset: u64) {
 		self.file_footer_offsets.insert(filenumber, fileoffset);
+	}
+
+	pub fn file_footer_segment_numbers(&self) -> &HashMap<u64, u64> {
+		&self.file_footer_segment_numbers
 	}
 
 	pub fn file_footer_offsets(&self) -> &HashMap<u64, u64> {
@@ -127,9 +234,11 @@ impl HeaderCoding for ObjectFooterLogical {
 	fn decode_content(data: Vec<u8>) -> Result<ObjectFooterLogical> {
 		let mut cursor = Cursor::new(data);
 		let footer_version = u8::decode_directly(&mut cursor)?;
+		let file_header_segment_numbers = HashMap::<u64, u64>::decode_directly(&mut cursor)?;
+		let file_header_offsets = HashMap::<u64, u64>::decode_directly(&mut cursor)?;
 		let file_footer_segment_numbers = HashMap::<u64, u64>::decode_directly(&mut cursor)?;
 		let file_footer_offsets = HashMap::<u64, u64>::decode_directly(&mut cursor)?;
-		Ok(ObjectFooterLogical::new(footer_version, file_footer_segment_numbers, file_footer_offsets))
+		Ok(ObjectFooterLogical::new(footer_version, file_header_segment_numbers, file_header_offsets, file_footer_segment_numbers, file_footer_offsets))
 	}
 
 }
